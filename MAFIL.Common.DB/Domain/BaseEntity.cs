2a1886e9a5aca2acffb718898d881d33f9b2ea6c
Navaneeth Sivaprasad <Navaneeth.as22295@gmail.com>
2023-09-07 11:10:51 +0530

    commit changes

--- MAFIL.Common.DB/Domain/BaseEntity.cs
+++ MAFIL.Common.DB/Domain/BaseEntity.cs
@@ -9,20 +9,20 @@ namespace MAFIL.Common.DB.Domain
 {
     public class BaseEntity
     {
-        public BaseEntity()
-        {
-            this.Created_On = new DateTime();
-            this.Update_On = new DateTime();
-        }
+        //public BaseEntity()
+        //{
+        //    this.Created_On = new DateTime();
+        //    this.Update_On = new DateTime();
+        //}
         [Key]
         [Required]
         public int Id { get; set; }
-        public string Name { get; set; }
-        public int Status { get; set; }
-        public int Created_By { get; set; }
-        public DateTime Created_On { get; set; }
-        public int? Update_By { get; set; }
-        public DateTime? Update_On { get; set; }
+        //public string Name { get; set; }
+        //public int Status { get; set; }
+        //public int Created_By { get; set; }
+        //public DateTime Created_On { get; set; }
+        //public int? Update_By { get; set; }
+        //public DateTime? Update_On { get; set; }
 
 
 
